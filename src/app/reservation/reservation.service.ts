08e8583f12197ed075a05ba07b1437dbff4de526
srijan_singh <singhsrijan809@gmail.com>
2024-05-27 10:17:48 +0530

    changes for mock appi

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,45 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/reservations';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
+  private apiUrl = "http://loaclhost:3000";
   private reservations: Reservation[] = [];
 
   // constructor gets loaded before ngOnit lifeCycle hook, To handle al the instances of the variables created 
-  constructor() {
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations? JSON.parse(savedReservations): [];
+  constructor(private http: HttpClient){
+
   }
 
   //CRUD operations
 
-  getReservations(): Reservation[] {
-    return this.reservations;
-    localStorage
+  getReservations(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>(this.apiUrl + "/reservations");
   }
 
-  getReservation(id: string): Reservation | undefined {
+  getReservation(id: string): Reservation | undefined { 
     return this.reservations.find(res => res.id === id);
   }
 
   addReservation(reservation: Reservation): void  {
     reservation.id = Date.now().toString();
     this.reservations.push(reservation);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void{
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1); 
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string, updatedReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
     this.reservations[index] = updatedReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 }
